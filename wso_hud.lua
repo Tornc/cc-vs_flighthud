bc38878cb38e5238773f03d06c6d1e882392251d
Tornc <110984950+Tornc@users.noreply.github.com>
2024-07-13 01:07:36 +0200

    MIDNIGHT CODING MIDNIGHT CODING

--- wso_hud.lua
+++ wso_hud.lua
@@ -1,4 +1,5 @@
 -- TEST: REMOVE LATER
+local ship = require("fake_ShipAPI")
 periphemu.create("front", "monitor")
 periphemu.create("top", "modem")
 
@@ -30,10 +31,20 @@ local SCREEN_WIDTH, SCREEN_HEIGHT = 15, 10
 ]]
 
 local current_time = 0
-
+local plane = {
+    x = 0, -- Position
+    y = 0,
+    z = 0,
+    vx = 0, -- Velocity
+    vy = 0,
+    vz = 0,
+    yaw = 0, -- Orientation
+    pitch = 0,
+    roll = 0,
+}
 local inbox, old_inbox = {}, {}
 local outgoing_message = { MY_ID }
-
+local targets = {}
 local mouse_x, mouse_y = 0, 0
 
 --[[
@@ -106,7 +117,7 @@ local function input_handler()
 end
 
 --[[
-    UI
+    HUD
 ]]
 
 local function write_at(x, y, text, colour)
@@ -115,6 +126,43 @@ local function write_at(x, y, text, colour)
     MONITOR.setCursorPos(x, y)
     MONITOR.write(text)
     MONITOR.setTextColour(previous_colour)
+end
+
+-- Consider this function as copied from Endal
+local line_types = { "\xAF", "-", "_", "|" } -- High, middle, low, vertical
+local function plot_line(x0, y0, x1, y1)
+    y1 = math.floor(y1 * (#line_types - 1) + 1)
+    y0 = math.floor(y0 * (#line_types - 1) + 1)
+    x1 = math.floor(x1)
+    x0 = math.floor(x0)
+    local dx = math.abs(x1 - x0)
+    local sx = x0 < x1 and 1 or -1
+    local dy = -math.abs(y1 - y0)
+    local sy = y0 < y1 and 1 or -1
+    local error = dx + dy
+
+    while true do
+        local char = dx < 3 and
+            line_types[#line_types] or
+            line_types[math.floor(y0 % (#line_types - 1)) + 1]
+        write_at(
+            x0,
+            math.floor(y0 / (#line_types - 1)),
+            char
+        )
+        if x0 == x1 and y0 == y1 then break end
+        local e2 = 2 * error
+        if e2 >= dy then
+            if x0 == x1 then break end
+            error = error + dy
+            x0 = x0 + sx
+        end
+        if e2 <= dx then
+            if y0 == y1 then break end
+            error = error + dx
+            y0 = y0 + sy
+        end
+    end
 end
 
 local function button()
@@ -143,14 +191,17 @@ local function button()
 
     self.draw = function()
         for i, line in ipairs(self.lines) do
-            write_at(self.x0, self.y0 + i - 1, line)
+            write_at(self.x0, self.y0 + i - 1, line, self.colour)
         end
     end
 
     return self
 end
 
-local function ui_displayer()
+local function confirm_screen()
+end
+
+local function HUD_displayer()
     if not MONITOR then return end
     print("Monitor attached.")
     -- TEST: UNCOMMENT LATER
@@ -161,17 +212,19 @@ local function ui_displayer()
     MONITOR.setPaletteColour(colours.lime, HUD_TEXT_COLOUR)
     MONITOR.setTextColour(colours.lime)
 
-    local PREVIOUS_BUTTON = button().create(1, 10, "\xABPRV")
-    local NEXT_BUTTON = button().create(12, 10, "NXT\xBB")
+    local PREVIOUS_BUTTON = button().create(1, 10, "[PRV]")
+    local NEXT_BUTTON = button().create(11, 10, "[NXT]")
+    local REMOVE_BUTTON = button().create(6, 5, "[RMV]", colours.red)
     while true do
         MONITOR.clear()
 
         PREVIOUS_BUTTON.draw()
         NEXT_BUTTON.draw()
+        REMOVE_BUTTON.draw()
 
         term.clear()
-        print(mouse_x , mouse_y)
-        print(PREVIOUS_BUTTON.is_clicked(), NEXT_BUTTON.is_clicked())
+        print(mouse_x, mouse_y)
+        print(PREVIOUS_BUTTON.is_clicked(), NEXT_BUTTON.is_clicked(), REMOVE_BUTTON.is_clicked())
 
         sleep(DELTA_TICK / 20)
     end
@@ -186,11 +239,22 @@ local function update_information()
 end
 
 local function update_state()
+    -- TEST: remove later
+    targets = { { 100, 200, 300 }, { 30, 20, 110 } }
     while true do
+        -- TEST: REMOVE LATER
+        ship.run(DELTA_TICK)
+
         update_information()
         current_time = current_time + DELTA_TICK
         sleep(DELTA_TICK / 20)
     end
 end
 
-parallel.waitForAll(update_state, ui_displayer, input_handler, message_handler)
+parallel.waitForAll(update_state, HUD_displayer, input_handler, message_handler)
+
+
+-- TODO: display information --> what do we need?
+-- target xyz, distance ship to target xyz, current speed(?), ETA (area close enough to target)
+-- delta yaw, pitch, roll(?)
+-- option to remove target (NEEDS a confirm)
